c5dee4d6bbaf630ef4ed80503b66aef8c36fb11c
‘Ama_lion’ <bahamadu2019@gmail.com>
2020-08-26 10:51:22 +0200

    finished

---
+++ src/Contentful.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const contentful = require("contentful");
+
+export default contentful.createClient({
+  space: "nuzdh1r71vz0",
+  accessToken: "8gQOS_Jj5_bnRNrlsSwLaEVWLZb7kjfavLfemeZbnCk",
+});
